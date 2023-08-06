9c0b4ec443ba863e875e23209b745e75fdc54699
Harsha Gupta <guptaharsha.harsha@gmail.com>
2023-08-06 08:48:33 +0530

    Deleted some extra-lines

--- Server/opponentfriend.cjs
+++ Server/opponentfriend.cjs
@@ -5,15 +5,7 @@ const io = require("socket.io")(3000, {
 });
 
 io.on("connection", (socket) => {
-  console.log("New Socket Connection");
   socket.on("move", function (from, to) {
     socket.broadcast.emit("recieve-move", from, to);
-  });
-  socket.on("turn-change", function (color) {
-    socket.broadcast.emit("color-change", color);
-  });
-  socket.on("color-assign", function (color) {
-    if (color == "white") socket.broadcast.emit("get-color", "black");
-    else socket.broadcast.emit("get-color", "white");
   });
 });
